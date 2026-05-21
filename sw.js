1d01bb18f21145364a7fd0448d71923cca17e423
kavishev046-dev <kavishev046@gmail.com>
2026-05-21 23:38:32 +0300

    offline mode over

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'kavisflora-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.dart.js',
+  '/flutter.js',
+  '/flutter_bootstrap.js',
+  '/manifest.json',
+  '/version.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
