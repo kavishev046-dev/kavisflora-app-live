ff8508c628a2b0e970f30b035fce21a3205bebdf
kavishev046-dev <kavishev046@gmail.com>
2026-05-22 18:42:53 +0300

    icone appdate

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1245096126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "308174999" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
