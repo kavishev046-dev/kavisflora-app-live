722f6941efe3ac74d4de90b2757ce7f7207bbad4
kavishev046-dev <kavishev046@gmail.com>
2026-06-16 23:10:22 +0300

    changes
    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "921465436" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3822128097" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
